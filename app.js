332972ddb9cce58b6f2e5a6a029e58bb83fef0f6
Ramesh Rukshan <ramesharukshan3@gmail.com>
2024-11-13 23:04:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const html = `
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Hi Hi Hiiiiiiiiii RAR!</title>
+    <title>Hi!</title>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
     <script>
       setTimeout(() => {
@@ -54,7 +54,8 @@ const html = `
   </head>
   <body>
     <section>
-      Hiiiiiii
+      Hiiiiiii<br>
+      Bla Bla Bla Bla
     </section>
   </body>
 </html>
