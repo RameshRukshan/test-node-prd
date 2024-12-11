56c687f56f85c2b275389c8184e05ed359e9574b
Ramesh Rukshan <ramesharukshan3@gmail.com>
2024-12-11 23:23:30 +0530

    port change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const pool = require('./src/config/database'); // Import the database pool
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Middleware to parse JSON
 app.use(express.json());
